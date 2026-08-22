c0e0fa9fa8cc277eae1f96324e06b154d0572a97
Stavroni <140117269+Stavroni@users.noreply.github.com>
2026-08-22 21:09:30 +1200

    Added time and date + sccp. Ui for select is also complete but functionality is not yet added.

--- a1_texteditor/src/main/java/a1/texteditor/EditorPanel.java
+++ a1_texteditor/src/main/java/a1/texteditor/EditorPanel.java
@@ -163,8 +163,10 @@ public class EditorPanel extends JPanel {
     //------------------------------------------------------------
     //helpfull textpane documentation; https://www.geeksforgeeks.org/java/java-jtextpane/
 
+    //idk what select text could otherwise imply cuz you have to hover over text to select it anyway, thus making a
+    //  select text which saves the text as e.g a string to be used by copy/cut redundant
     public void selectText() {
-        textPane.selectAll();
+        textPane.getSelectedText();
     }
 
     public void cutText() {
